af90b946557b9c6b764b30f49858c8468118d298
Asko Soukka <asko.soukka@iki.fi>
2012-10-04 07:05:36 +0300

    Added imperative configuration support for games and refactored Numbers to be registered using it.

--- src/working_memory_games/__init__.py
+++ src/working_memory_games/__init__.py
@@ -1,12 +1,44 @@
 #-*- coding: utf-8 -*-
 """ Pyramid startup """
 
+import venusian
+
 from pyramid.config import Configurator
 
 from working_memory_games.app import Application
+from working_memory_games.interfaces import IPlayer, IGame
 
 import logging
 logger = logging.getLogger("working_memory_games")
+
+
+class game_config(object):
+    """ A class decorator which allows a developer to config game registrations
+    nearer to actual code. """
+
+    def __init__(self, **settings):
+        self.__dict__.update(settings)
+
+    def __call__(self, wrapped):
+        settings = self.__dict__.copy()
+
+        def callback(context, name, ob):
+            config = context.config.with_package(info.module)
+
+            # Register game
+            config.registry.registerAdapter(ob, name=ob.name,
+                                            required=(IPlayer,),
+                                            provided=IGame)
+
+            if settings.get("add_view", True):
+                config.add_view(context=ob, renderer="%s.pt" % ob.name)
+
+            if settings.get("add_static_view", True):
+                config.add_static_view("%s/static" % ob.name, path=ob.name)
+
+        info = venusian.attach(wrapped, callback, category="pyramid")
+
+        return wrapped
 
 
 def main(global_config, **settings):
@@ -17,7 +49,7 @@ def main(global_config, **settings):
     # Register robots.txt and favicon.ico
     config.include("pyramid_assetviews")
     config.add_asset_views("working_memory_games:",  # requires package name
-                           filenames=['robots.txt', 'favicon.ico'])
+                           filenames=["robots.txt", "favicon.ico"])
 
     # Enable ZODB support
     config.include("pyramid_zodbconn")
@@ -26,6 +58,9 @@ def main(global_config, **settings):
     # Enable ZCML support
     config.include("pyramid_zcml")
     config.load_zcml("configure.zcml")
+
+    # Enable imperative configuration for games
+    config.scan(".games")
 
     # Make WSGI
     return config.make_wsgi_app()
--- src/working_memory_games/games/numbers.py
+++ src/working_memory_games/games/numbers.py
@@ -3,13 +3,17 @@
 
 import random
 
+from pyramid.view import view_config
+
 from zope.interface import implements
 from zope.interface.verify import verifyObject
 
+from working_memory_games import game_config
 from working_memory_games.interfaces import IGame
 from working_memory_games.games import Game
 
 
+@game_config(add_view=True, add_static_view=True)
 class Numbers(Game):
     """ Numbers """
 
@@ -19,6 +23,7 @@ class Numbers(Game):
     title = u"Numeroita"
 
 
+@view_config(name="new", context=Numbers, renderer="json", xhr=True)
 def new_game(context, request):
     """ Return new game data """
 
