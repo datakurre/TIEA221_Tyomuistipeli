72304850a6fdac5d01fcc40304c4454cbee9202d
Asko Soukka <asko.soukka@iki.fi>
2012-10-31 08:46:25 +0200

    Added AJAX-request for players.

--- src/working_memory_games/static/root.js
+++ src/working_memory_games/static/root.js
@@ -1,25 +1,27 @@
-
-
-
-$(document).ready(function(){
+jQuery(function($) {
 
     var title = $('h1').text(), idx;
     var newTitle = '';
     for (idx=0; idx<title.length; idx+=1) {
-	newTitle += '<div class="color-'+(idx%5)+'">'+title.charAt(idx) +'</div>';
+        newTitle += '<div class="color-'+(idx%5)+'">'+title.charAt(idx) +'</div>';
     }
     $('h1').html(newTitle);
 
     $('h1').children().each(function(idx){
-	$(this)
-	    .delay(idx*100)
-	    .animate({'top':'-80px'}, 300)
-	    .animate({'top':'0px'}, 400+idx*50);
+        $(this)
+            .delay(idx*100)
+            .animate({'top':'-80px'}, 300)
+            .animate({'top':'0px'}, 400+idx*50);
     });
 
-    // bind buttons
-    $('#join').click(function(event){
-	$(this).slideUp();
+    // load players
+    $.get("list_players", function(data) {
+        $("#majorRow .center").prepend(data);
+
+        // bind buttons
+        $('#join').click(function(event){
+            $(this).slideUp();
+        });
     });
 
 });
