c8633e200a2cf54fefa79b6482f9d9332130cca2
Matti Katila <matti.katila@gmail.com>
2013-04-07 12:03:55 +0300

    Add feedback music playing

--- src/working_memory_games/js/game.js
+++ src/working_memory_games/js/game.js
@@ -2,6 +2,7 @@ var global = global || {}
 global.userid = 0;
 global.gameItems = [];
 global.userItems = [];
+global.base_ctx = $('meta[name=base]').attr("content");
 global.ctx = $('meta[name=context]').attr("content");
 global.callbacks = {
     newGame: function(){},
@@ -50,6 +51,28 @@ function GameCheckUserPress(item) {
     GameCorrectAnswer();
 }
 
+function GamePlayFeedback(passed) {
+    var name = '';
+    if (passed) {
+	rights = [];
+	for (var i=1; i<=15; i++) {
+	    rights.push('right'+i);
+	}
+	name = rights[Math.floor(Math.random() * rights.length)];
+    } else {
+	wrongs = [];
+	for (var i=1; i<=12; i++) {
+	    wrongs.push('wrong'+i);
+	}
+	name = wrongs[Math.floor(Math.random() * wrongs.length)];
+    }
+
+    $.preload(name, global.base_ctx + '/snd/' + name + '.[mp3,ogg]');
+    $('body').on('preloaded', function() {
+	$('body').play(name).promise().done(function() {});
+    });
+}
+
 function createDialog(data){
   var dialog;
   if ($('.modal').length === 0) {
@@ -68,20 +91,26 @@ function createDialog(data){
 
 function GameCorrectAnswer() {
   $.ajax({
-    type: 'POST',
-    url: global.ctx + '/pass',
-    data: JSON.stringify({game: global.gameItems, user: global.userItems}),
-    contentType: 'application/json; charset=utf-8',
-    success: function(data) { createDialog(data); }
+      type: 'POST',
+      url: global.ctx + '/pass',
+      data: JSON.stringify({game: global.gameItems, user: global.userItems}),
+      contentType: 'application/json; charset=utf-8',
+      success: function(data) { 
+	  GamePlayFeedback(true);
+	  createDialog(data); 
+      }
   });
 }
 
 function GameIncorrectAnswer() {
   $.ajax({
-    type: 'POST',
-    url: global.ctx + '/fail',
-    data: JSON.stringify({game: global.gameItems, user: global.userItems}),
-    contentType: 'application/json; charset=utf-8',
-    success: function(data) { createDialog(data); }
+      type: 'POST',
+      url: global.ctx + '/fail',
+      data: JSON.stringify({game: global.gameItems, user: global.userItems}),
+      contentType: 'application/json; charset=utf-8',
+      success: function(data) {
+	  GamePlayFeedback(false);
+	  createDialog(data);
+      }
   });
 }
