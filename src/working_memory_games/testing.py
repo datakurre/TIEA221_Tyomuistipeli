8cccac8ab292f3244c32972dbe0a292893c7472d
Asko Soukka <asko.soukka@iki.fi>
2013-05-24 17:16:20 +0300

    Fix test fixtures

--- src/working_memory_games/testing.py
+++ src/working_memory_games/testing.py
@@ -3,6 +3,10 @@
 
 import os
 
+from pyramid.renderers import JSON
+
+json_renderer = JSON()
+
 from plone.testing import Layer
 
 from pyramid import testing
@@ -12,50 +16,7 @@ from pyramid.config import Configurator
 from working_memory_games import static_file
 from working_memory_games.app import Application
 
-
-def main(global_config, **settings):
-    """ This function returns a Pyramid WSGI application with mock database """
-
-    # Create Pyramid configurator
-    config = Configurator(settings=settings)
-
-    # Register robots.txt, humans.txt  and favicon.ico
-    for filename in ["robots.txt", "humans.txt", "favicon.ico"]:
-        path = os.path.join(os.path.dirname(__file__), filename)
-        config.add_route(path, "/%s" % filename)
-        config.add_view(route_name=path, view=static_file(path))
-
-    # Configure common static resources
-    config.add_static_view(name="css", path="css")
-    config.add_static_view(name="img", path="img")
-    config.add_static_view(name="js", path="js")
-    config.add_static_view(name="lib", path="lib")
-
-    # Register Chameleon rendederer also for .html-files
-    config.add_renderer(".html", "pyramid.chameleon_zpt.renderer_factory")
-
-    # Configure common static resources
-    config.add_static_view(name="bootstrap", path="bootstrap")
-    config.add_static_view(name="static", path="static")
-
-    # Configure common direct routes, which takes precedence over traverse
-    config.add_route("root", "/")
-    config.add_route("register", "/liity", request_method="GET")
-
-    # Scan app views for their configuration
-    config.scan(".app")
-
-    # Scan games for their configuration
-    config.scan(".games")
-
-    # Note: No ZODB for testing!
-
-    # Configure traverse (for views that require access to the database)
-    config.add_route("traversal", "/*traverse",
-                     factory=lambda request: Application(request, {}))
-
-    # Make WSGI app
-    return config.make_wsgi_app()
+from working_memory_games import main
 
 
 class PyramidLayer(Layer):
@@ -63,12 +24,19 @@ class PyramidLayer(Layer):
     def setUp(self):
         self['config'] = testing.setUp(settings={})
 
-        # Register Chameleon rendederer also for .html-files
-        self['config'].add_renderer(".html",
-                                    "pyramid.chameleon_zpt.renderer_factory")
+        self['config'].add_renderer(
+                ".html", "pyramid.chameleon_zpt.renderer_factory")
+        self['config'].add_renderer(
+                "json", json_renderer)
+
+        self['config'].add_route("root", "/")
+        self['config'].add_route("register", "/liity", request_method="GET")
+
+        self['config'].add_route("traversal", "/*traverse", factory=Application)
 
         self['config'].scan("working_memory_games.app")
         self['config'].scan("working_memory_games.games")
+
 
     def tearDown(self):
         testing.tearDown()
@@ -82,10 +50,27 @@ class PyramidLayer(Layer):
 INTEGRATION_TESTING = PyramidLayer()
 
 
+class PyramidAppLayer(Layer):
+
+    def setUp(self):
+        self['app'] = main({}, testing=True)
+
+    def tearDown(self):
+        testing.tearDown()
+
+    def testSetUp(self):
+        pass
+
+    def testTearDown(self):
+        pass
+
+FUNCTIONAL_TESTING = PyramidAppLayer()
+
+
 class PyramidServerLayer(Layer):
 
     def setUp(self):
-        self['app'] = main({})
+        self['app'] = main({}, testing=True)
 
         # XXX: If the server needs a DB, set up test DB here.
 
