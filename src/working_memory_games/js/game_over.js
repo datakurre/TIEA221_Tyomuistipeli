bf3f3dc1a27f1312720a2f43d77d6ceea54932e5
Matti Katila <matti.katila@gmail.com>
2013-07-21 01:05:50 +0300

    add info to play tomorrow

--- src/working_memory_games/js/game_over.js
+++ src/working_memory_games/js/game_over.js
@@ -2,11 +2,13 @@
 $(document).ready(function(){
     $.preload('hugoism',
               global.base_ctx + '/snd/hugoismit/hugoismi'+(Math.floor(Math.random()*19))+'.[ogg,mp3]');
+    $.preload('huomenna',
+              global.base_ctx + '/snd/pelataan_taas_huomenna.[ogg,mp3]');
     $.preload('lopuke',
               global.base_ctx + '/snd/lopuke.[ogg,mp3]');
     $('body').one('preloaded', function(){
         $('body').play('hugoism');
         if ($('#nextGame').attr('href') == global.base_ctx + '/')
-            $('body').play('lopuke');
+            $('body').play('huomenna').play('lopuke');
     });
 });
