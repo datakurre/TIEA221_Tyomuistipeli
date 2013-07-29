500f81a7461d89b0690971a98f9d7033e533d54f
Asko Soukka <asko.soukka@iki.fi>
2013-07-29 11:22:50 +0300

    Enable big red button on iPad

--- src/working_memory_games/js/game_iframe.js
+++ src/working_memory_games/js/game_iframe.js
@@ -1,11 +1,12 @@
 jQuery(function($) {
-    var checkWindowHeight, createGameFrame, timeout = null, dialog = null;
+    var checkWindowHeight, createGameFrame, updateViewport,
+        timeout = null, dialog = null;
 
     checkWindowHeight = function() {
         var windowHeight = $(window).height(),
             platform = {
-                "Windows": "windows"
-            }[BrowserDetect.OS] || "windows";
+                'Windows': 'windows'
+            }[BrowserDetect.OS] || 'windows';
 
         if (windowHeight < 768) {
             if (dialog === null) {
@@ -13,7 +14,7 @@ jQuery(function($) {
                           .css('display', 'block'), {
                     fitViewport: true,
                     closeOverlay: false,
-                    closeSelector: ".pure-button-primary",
+                    closeSelector: '.pure-button-primary',
                     closeKeyCode: null,
                     closeText: ''
                 });
@@ -40,7 +41,7 @@ jQuery(function($) {
             url: base_url + '/next_game',
             contentType: 'application/json; charset=utf-8',
             success: function(data, textStatus, jqXHR) {
-                var contentType = jqXHR.getResponseHeader("Content-Type");
+                var contentType = jqXHR.getResponseHeader('Content-Type');
                 if (/^application\/json.*/.test(contentType)) {
                     $('<iframe></iframe>')
                         .attr('id', 'game-iframe')
@@ -57,9 +58,7 @@ jQuery(function($) {
         });
     };
 
-    checkWindowHeight();
-
-    function doOnOrientationChange() {
+    updateViewport = function() {
         $('head meta[name="viewport"]').remove();
         switch(window.orientation) {
             case -90:
@@ -71,11 +70,48 @@ jQuery(function($) {
                 $('head').append($('<meta name="viewport" content="height=device-height, width=768, initial-scale=1.0, user-scalable=no">'));
                 break;
         }
+    };
+
+    // Check windows height
+    if (BrowserDetect.OS !== 'iPad') {
+        checkWindowHeight();
     }
 
-    // Limit doOrientationChange only for iPad, for which it is optimized:
-    if (BrowserDetect.OS === "iPad") {
-        window.addEventListener('onorientationchange', doOnOrientationChange);
-        doOnOrientationChange();
+    // Limit setViewportOnRotation only for iPad, for which it is optimized:
+    if (BrowserDetect.OS === 'iPad') {
+        window.addEventListener('onorientationchange', updateViewport);
+        updateViewport();
+    }
+
+    // Show big red button for iPad
+    if (BrowserDetect.OS === 'iPad') {
+        dialog = $.modal($('#touchstart').clone()
+            .css('display', 'block')
+            .one('touchstart', function() {
+                if ($._preload !== undefined && $._preload.requireTouchStart) {
+                    if ($._preload.context) {
+                        $._preload.source =
+                            $._preload.context.createBufferSource();
+                        $._preload.source.connect(
+                            $._preload.context.destination);
+                        $._preload.source.noteOn(0);
+                    } else {
+                        $._preload.audio.play();
+                    }
+                    $._preload.requireTouchStart = false;
+                }
+                return true;
+            }), {
+            width: 708,
+            height: 566,
+            fitViewport: true,
+            closeOverlay: false,
+            closeSelector: "#big-red-button",
+            closeKeyCode: null,
+            closeText: ''
+        });
+        $(window.document).one('modalAfterClose', function() {
+            createGameFrame();
+        });
     }
 });
